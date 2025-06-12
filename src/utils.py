57713536f2a770ddba5001059cdc16c1586eca0c
arjunverma2004 <163740749+arjunverma2004@users.noreply.github.com>
2025-06-12 21:33:51 +0530

    ..

--- src/utils.py
+++ src/utils.py
@@ -3,7 +3,7 @@ import sys
 
 import numpy as np 
 import pandas as pd
-import dill
+import dill #more power than pickle
 import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
